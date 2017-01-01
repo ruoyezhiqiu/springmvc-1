4dc1f7249488084a1a22a3e887f57d53dba3e77d
ruoyezhiqiu <18380231337@163.com>
2017-01-01 22:14:40 +0800

    update

---
+++ src/com/wushengde/springmvc/handlers/SpringMVCTest.java
@@ -0,0 +1,12 @@
+package com.wushengde.springmvc.handlers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SpringMVCTest {
+	
+	public String testRequestMapping(){
+		System.out.println("testRequestMapping");
+		
+	}
+}
